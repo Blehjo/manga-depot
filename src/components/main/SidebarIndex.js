71275c2672e517792d4b235bde68b1d4e54bb22e
bseton11-YJKnqQ <bseton11@gmail.com>
2022-09-24 14:36:45 -0500

    updated location of components

--- src/components/main/SidebarIndex.js
+++ src/components/main/SidebarIndex.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import SidebarOverlay from '../Pieces/SidebarOverlay';
-import SidebarMenu from '../Pieces/SidebarMenu';
+import SidebarOverlay from '../pieces/SidebarOverlay';
+import SidebarMenu from '../pieces/SidebarMenu';
 import Collapse from 'react-bootstrap/Collapse';
 
 function SidebarIndex(props) {
