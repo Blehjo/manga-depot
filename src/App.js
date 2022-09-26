c88e94128369eb409158aa50de462b0c874fd3de
Blehjo <blehjooo@gmail.com>
2022-09-26 13:06:12 -0500

    fixed error in App.js where path to Container was incorrect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Container from './components/Container';
+import Container from './components/main/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
