c488cbeca3a79872be90772b4651ce115c76d6ef
Blehjo <blehjooo@gmail.com>
2022-10-03 11:59:03 -0500

    updated cards. they now display platform and handle errors

--- src/components/main/Body.js
+++ src/components/main/Body.js
@@ -3,15 +3,16 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { BookFill, HeartFill } from 'react-bootstrap-icons';
 import { Button } from 'react-bootstrap';
+import unixConverter from '../../utils/Date';
 
 function Body(props) {
   return (
     <>
     <Row xs={1} sm={1} md={2} lg={3} xl={4} className="g-4 pt-3" key={1}>
-      {props.results.map((result) => (
+      {props.results?.map((result) => (
         <Col className='' key={result.id}>
-          <Card className="" key={result.id}>
-            {/* {result.cover.image_id !== undefined ? <Card.Img className='img-fluid' variant="top" src={`https://images.igdb.com/igdb/image/upload/t_1080p/${result.cover.image_id}.jpg`} /> : <p>hello</p>} */}
+          <Card style={{ }} className="h-100" key={result.id}>
+            {<Card.Img className='img-fluid' variant="top" src={`https://images.igdb.com/igdb/image/upload/t_1080p/${result.cover?.image_id}.jpg`} />}
             <Card.Body>
               <Card.Title>
                 <Card.Link className='card-info' href={`/${result.name}`}>
@@ -19,10 +20,13 @@ function Body(props) {
                 </Card.Link>
               </Card.Title>
               <Card.Text>
-                {typeof result.first_release_date != undefined ? result.first_release_date : <p>no release date</p>}
-                {result.platforms}
-                {result.rating}
-                {/* {result.storyline} */}
+                {unixConverter(result.first_release_date)}
+              </Card.Text>
+              <Card.Text>
+                {result.rating !== undefined ? `Rating: ${Math.round(result.rating)}` : ''}
+              </Card.Text>
+              <Card.Text>
+                {`Platforms: ${result.platforms?.map((element) => (element.abbreviation))}`}
               </Card.Text>
               <div className='collect'>
                 <Button variant='dark'>
--- src/utils/Date.js
+++ src/utils/Date.js
@@ -1,8 +1,6 @@
 import moment from "moment";
 
 export default function unixConverter(timestamp) {
-    var t = new Date();
-    t.setSeconds(timestamp);
-    var formatted = moment(t).format("dd.mm.yyyy hh:MM:ss");
-    return formatted;
+    let convertedTime = moment.unix(timestamp).format('ll');
+    return `Released: ${convertedTime}`;
 }
