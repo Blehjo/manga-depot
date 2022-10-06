a2e8fb1bb66c75b26cf809d23ed2812ef808f88f
bseton11-YJKnqQ <bseton11@gmail.com>
2022-10-06 13:00:19 -0500

    added icons to the cards, so user interaction can be simpler

--- src/components/main/Body.js
+++ src/components/main/Body.js
@@ -4,39 +4,47 @@ import Row from 'react-bootstrap/Row';
 import { BookFill, HeartFill } from 'react-bootstrap-icons';
 import { Button } from 'react-bootstrap';
 import unixConverter from '../../utils/Date';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faDownload, faCommentAlt, faRetweet, faHeart } from '@fortawesome/free-solid-svg-icons'
 
-function Body(props) {
+
+
+function Body({ results }) {
   return (
     <>
     <Row xs={1} sm={1} md={2} lg={3} xl={4} className="g-4 pt-3" key={1}>
-      {props.results?.map((result) => (
+      {results?.map((result) => (
         <Col className='' key={result.id}>
-          <Card style={{ }} className="h-100" key={result.id}>
-            {<Card.Img className='img-fluid' variant="top" src={`https://images.igdb.com/igdb/image/upload/t_1080p/${result.cover?.image_id}.jpg`} />}
-            <Card.Body className=''>
-              <Card.Title>
-                <Card.Link className='card-info' href={`/${result.name}`}>
-                  {result.name}
-                </Card.Link>
-              </Card.Title>
-              <Card.Text>
-                {unixConverter(result.first_release_date)}
-              </Card.Text>
-              <Card.Text>
-                {result.rating !== undefined ? `Rating: ${Math.round(result.rating)}` : ''}
-              </Card.Text>
-              <Card.Text>
-                {`Platforms: ${result.platforms?.map((element) => (`\n${element.abbreviation}`))}`}
-              </Card.Text>
-              <div className='collect'>
-                <Button variant='dark'>
-                  <BookFill className='m-2' color='rgb(97, 219, 251)' size={40}/>
-                </Button>
-                <Button variant='dark'>
-                  <HeartFill className='m-2' color='rgb(97, 219, 251)' size={40}/>
-                </Button>
+          <Card style={{ }} className="bg-dark card-container h-100" key={result.id}>
+            <div className='card-container'>
+            <Card.Link className='card-info' href={`/${result.name}`}>
+              {<Card.Img className='img-fluid' variant="top" src={`https://images.igdb.com/igdb/image/upload/t_1080p/${result.cover?.image_id}.jpg`} />}
+            </Card.Link>
+              <Card.ImgOverlay>
+                <Card.Text>
+                  <FontAwesomeIcon className="icon" icon={faEye} />
+                </Card.Text>
+                <Card.Text className='icon2'>
+                  <FontAwesomeIcon className="icon-item" icon={faHeart} />
+                  <FontAwesomeIcon className="icon-item" icon={faCommentAlt} />
+                  <FontAwesomeIcon className="icon-item" icon={faRetweet} />
+                </Card.Text>
+                <Card.Text className="icon3">
+                  <FontAwesomeIcon className="" icon={faDownload} />
+                </Card.Text>
+              </Card.ImgOverlay>
               </div>
-            </Card.Body>
+              <Card.Body className=''>
+                <Card.Text>
+                  {unixConverter(result.first_release_date)}
+                </Card.Text>
+                <Card.Text>
+                  {result.rating !== undefined ? `Rating: ${Math.round(result.rating)}` : ''}
+                </Card.Text>
+                <Card.Text>
+                  {`Platforms: ${result.platforms?.map((element) => (`\n${element.abbreviation}`))}`}
+                </Card.Text>
+              </Card.Body>
           </Card>
         </Col>
       ))}
