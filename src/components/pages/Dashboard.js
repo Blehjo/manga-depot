754b459b4bef7debf4039c35165a0d25ab35ca06
bseton11-YJKnqQ <bseton11@gmail.com>
2022-11-23 23:40:41 -0600

    added a logo and added it to index.html

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 const Dashboard = () => {
+    function getUsers() {
+        fetch(`/`)
+        .then((response) => response.json())
+        .then(users => console.log(users));
+    }
+
+    const users = getUsers();
     return (
         <>
             <h1>Dashboard</h1>
+            <div>
+                {getUsers()}
+            </div>
         </>
     )
 }
