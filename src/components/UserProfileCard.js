056ccef8080d4eb0744248fbfef1679a635b264f
Blehjo <blehjooo@gmail.com>
2022-12-14 20:04:12 -0600

    error handling components

--- src/components/UserProfileCard.js
+++ src/components/UserProfileCard.js
@@ -21,7 +21,7 @@ const UserProfileCard = () => {
         <Fragment>
             {profile?.map(({ id, about, first_name, country, friendships, games, media_location, username, userposts, groups }) => (
             <Card style={{ color: 'white' }} className="bg-dark" key={id}>
-                    <Card.Img variant="top" src={media_location ? require(media_location) : "https://www.cooperhewitt.org/wp-content/uploads/2018/07/20914_472d45b4ae377c5f_b1.jpg"} /> 
+                <Card.Img variant="top" src={media_location ? require(media_location) : "https://www.cooperhewitt.org/wp-content/uploads/2018/07/20914_472d45b4ae377c5f_b1.jpg"} /> 
                 <Card.Body>
                     <Card.Title>{username}</Card.Title>
                     <Card.Subtitle>{first_name}</Card.Subtitle>
@@ -29,12 +29,12 @@ const UserProfileCard = () => {
                     <Card.Subtitle>{about}</Card.Subtitle>
                     <Card.Title>Groups</Card.Title>
                     {groups?.length > 0 ? groups?.map(({ group_name, media_location_url }) => (
-                        <Card className="bg-dark">
+                        <Card key='top' className="bg-dark">
                             <Card.Img src={media_location_url}/>
                             <Card.Title>{group_name}</Card.Title>
                         </Card>
                     )) : (
-                        <Card className="bg-dark">
+                        <Card key='bottom' className="bg-dark">
                             <Card.Text>Join a Group</Card.Text>
                         </Card>
                     )}
@@ -42,11 +42,11 @@ const UserProfileCard = () => {
                 <Card.Footer>
                     <Card.Title>Games</Card.Title>
                     {games?.length > 0 ? games?.map((game) => (
-                        <Card className="bg-dark" >
+                        <Card key='top'className="bg-dark" >
                             <Card.Text>{game.title}</Card.Text>
                         </Card>
                     )) : (
-                        <Card className="bg-dark">
+                        <Card key='bottom' className="bg-dark">
                             <Card.Text>No Games</Card.Text>
                         </Card>
                     )}
