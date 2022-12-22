95bc894f0ddd0f1906b20b68b33547925ad58cde
Blehjo <blehjooo@gmail.com>
2022-12-22 14:47:26 -0600

    updated profile page to check if loggedin before displaying profile page

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,12 +1,16 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect, useContext } from 'react';
 import { Row, Col} from 'react-bootstrap';
 import axios from 'axios';
 
 import ProfileCard from '../ProfileCard';
 import ProfileTabs from '../ProfileTabs';
 
+import { UserContext } from '../../contexts/user.context';
+import Authentication from './Authentication';
+
 export default function Profile() {
   const [profile, setProfile] = useState();
+  const { currentUser } = useContext(UserContext);
 
   useEffect( () => {
 
@@ -22,14 +26,20 @@ export default function Profile() {
 
   return (
     <Fragment>
-      <Row className="mediatoggle m-5" lg={2} xl={2}>
-        <Col md={4} lg={4} xl={4}>
-          <ProfileCard />
-        </Col>
-        <Col md={8} lg={8} xl={8}>
-          <ProfileTabs />
-        </Col>
-      </Row> 
+      {
+        !currentUser ? (
+          <Authentication />
+        ) : (
+          <Row className="mediatoggle m-5" lg={2} xl={2}>
+            <Col md={4} lg={4} xl={4}>
+              <ProfileCard />
+            </Col>
+            <Col md={8} lg={8} xl={8}>
+              <ProfileTabs />
+            </Col>
+          </Row> 
+        )
+      }
     </Fragment>
   );
 }
