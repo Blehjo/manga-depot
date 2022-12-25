9834ae70c1845a42b76d7eb61e034bd3f4ba345a
bseton11-YJKnqQ <bseton11@gmail.com>
2022-12-25 07:18:55 -0600

    added delete games feature to profilecard

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Fragment, useContext, useState } from "react";
 import { Card, Row, Col, Modal } from "react-bootstrap";
 import { AuthContext } from "../contexts/auth.context";
@@ -9,6 +10,14 @@ const ProfileCard = () => {
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const handleClickEvent = () => {
+        
+    }
+
+    const deleteGame = async (event) => {
+        await axios.delete(`/api/games/${event.target.id}`);
+    }
 
     return (
         <Fragment>
@@ -22,8 +31,15 @@ const ProfileCard = () => {
                     <Card.Subtitle>{about}</Card.Subtitle>
                     <Card.Title>Posts</Card.Title>
                     <Card.Text>{userposts.length}</Card.Text>
-                    <Card.Title>Shells</Card.Title>
-                    {groups?.length > 0 ? groups?.map(({ group_name, media_location_url }) => (
+                        <Row xs={2} sm={2} md={2} lg={2} xl={2}>
+                            <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+                                <Card.Title>Shells</Card.Title>
+                            </Col>
+                            <Col style={{ position: 'relative' }} xs={6} sm={6} md={6} lg={6} xl={6}>
+                                <Card.Title style={{ position: 'absolute', bottom: '0', fontSize: '75%' }} show={show} onHide={handleClose} onClick={handleShow}>Create a shell</Card.Title>
+                            </Col>
+                        </Row>
+                    {groups?.length > 0 && groups?.map(({ group_name, media_location_url }) => (
                         <Card className="m-1 bg-dark">
                             <Row xs={2} sm={2} md={2} lg={2} xl={2}>
                                 <Col style={{ width: '3rem' }} xs={1} sm={1} md={1} lg={1} xl={1}>
@@ -34,17 +50,7 @@ const ProfileCard = () => {
                                 </Col>
                             </Row>
                         </Card>
-                    )) : (
-                        <Card className="bg-dark">
-                            <Row xs={2} sm={2} md={2} lg={2} xl={2}>
-                                <Col xs={1} sm={1} md={1} lg={1} xl={1}>
-                                </Col>
-                                <Col>
-                                    <Card.Text show={show} onHide={handleClose} onClick={handleShow}>No Shells Yet</Card.Text>
-                                </Col>
-                            </Row>
-                        </Card>
-                    )}
+                    ))}
                     <Card.Title>Friends</Card.Title>
                     {friendships?.length > 0 ? groups?.map(({ group_name, media_location_url }) => (
                         <Card className="m-1 bg-dark">
@@ -63,7 +69,7 @@ const ProfileCard = () => {
                                 <Col xs={1} sm={1} md={1} lg={1} xl={1}>
                                 </Col>
                                 <Col>
-                                    <Card.Text show={show} onHide={handleClose} onClick={handleShow}>Create a Shell</Card.Text>
+                                    <Card.Text show={show} onHide={handleClose} onClick={handleShow}>Follow mates</Card.Text>
                                 </Col>
                             </Row>
                         </Card>
@@ -73,16 +79,23 @@ const ProfileCard = () => {
                     <Card.Title>Games</Card.Title>
                     {games?.length > 0 ? games?.map(({ id, media_location_url, title}) => (
                         <Card className="m-1 bg-dark" >
-                            <Card.Link style={{ textDecoration: 'none' }} href={`/games/${id}`}>
-                                <Row xs={2} sm={2} md={2} lg={2} xl={2}>
-                                    <Col xs={1} sm={1} md={1} lg={1} xl={1}>
-                                        <Card.Img style={{ width: '1rem'}} src={media_location_url} />
+                                <Row xs={3} sm={3} md={3} lg={3} xl={3}>
+                                    <Col xs={9} sm={9} md={9} lg={9} xl={9}>
+                                        <Card.Link style={{ textDecoration: 'none' }} href={`/games/${id}`}>
+                                            <Row xs={2} sm={2} md={2} lg={2} xl={2}>
+                                                <Col xs={1} sm={1} md={1} lg={1} xl={1}>
+                                                    <Card.Img style={{ width: '1rem'}} src={media_location_url} />
+                                                </Col>
+                                                <Col xs={10} sm={10} md={10} lg={10} xl={10} style={{ position: 'relative' }}>
+                                                    <Card.Text style={{ position: 'absolute', bottom: '0' }}>{title}</Card.Text>
+                                                </Col>
+                                            </Row>
+                                        </Card.Link>
                                     </Col>
-                                    <Col style={{ position: 'relative' }}>
-                                        <Card.Text style={{ position: 'absolute', bottom: '0' }}>{title}</Card.Text>
+                                    <Col xs={3} sm={3} md={3} lg={3} xl={3}>
+                                        <Card.Text id={id} onClick={deleteGame}>Delete</Card.Text>
                                     </Col>
                                 </Row>
-                            </Card.Link>
                         </Card>
                     )) : (
                         <Card className="bg-dark">
