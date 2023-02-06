1fc03e0bfbcf9534528cb37f9edd57099214a9a6
bseton11-YJKnqQ <bseton11@gmail.com>
2023-02-06 04:29:52 -0600

    updating currentuser dispatch

--- src/utils/userDocument.js
+++ src/utils/userDocument.js
@@ -23,17 +23,15 @@ export const login = async (email, password) => {
 }
 
 export const getUser = async () => {
-    await axios({
+    const response = await axios({
         method: 'get',
         url: "https://shellgeistapi.herokuapp.com/api/users/",
         headers: {
             'Content-Type': 'application/json',
         },
         withCredentials: true
-    })
-    .then((response) => {
-        return response.data;
     });
+    return response;
 }
 
 export const signUpUser = async (username, email, password, country, date_of_birth, first_name, last_name) => {
