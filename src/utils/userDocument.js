50180cd51eea25ee9fc7ee6a036478e50f8a5615
bseton11-YJKnqQ <bseton11@gmail.com>
2023-02-06 04:40:14 -0600

    updating currentuser dispatch

--- src/utils/userDocument.js
+++ src/utils/userDocument.js
@@ -31,8 +31,8 @@ export const getUser = async () => {
         },
         withCredentials: true
     })
+    return response
     .then((response) => response.data);
-    return response;
 }
 
 export const signUpUser = async (username, email, password, country, date_of_birth, first_name, last_name) => {
